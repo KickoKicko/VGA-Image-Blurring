e1d044ce7f9160a278c982b9e69e435b2ab29789
Finn Harkort-Berge <finnhb@kth.se>
2024-11-16 14:34:31 +0100

    small cleanup

--- Board Test Environment/vgaProgram.c
+++ Board Test Environment/vgaProgram.c
@@ -6,45 +6,30 @@ extern void print_dec(unsigned int);
 extern void display_string(char *);
 extern void enable_interrupt(void);
 
+void handle_interrupt(){
+
+}
+
 /* Add your code here for initializing interrupts. */
 void labinit(void)
 {
   // enable_interrupt();
 }
 
-uint8_t tempCreatePixelData()
-{
-  uint8_t tempPixelData[320 * 240];
-  for (int i = 0; i < 320 * 240; i++)
-  {
-    tempPixelData[i] = i;
-  }
-  return *tempPixelData;
-}
-
-void updateVGADisplay(uint8_t *pixelData)
+void updateVGADisplay()
 {
   volatile char *VGA = (volatile char *)0x08000000;
-  // Define a simple 16-color palette
-  unsigned char colorPalette[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-
-  // Loop through every pixel in the screen (320x240)
-  for (int i = 0; i < 320 * 240; i++)
-  {
-    // Set the pixel color (loop through the palette)
-    VGA[i] = colorPalette[i % 16]; // Use modulo to cycle through the 16 colors
-  }
-}
-
-char readJtag()
-{
-  volatile char *JTAG = (volatile char *)0x04000040;
-  return JTAG[0];
+    
+    // Loop through every pixel in the screen (320x240)
+    for (int i = 0; i < 320 * 240; i++)
+    {
+        // Set the pixel color (cycle through the palette)
+        VGA[i] = flowers_bmp[i];  // Use modulo to cycle through the 256 colors
+    }
 }
 
 /* Your code goes into main as well as any needed functions. */
 int main(void)
 {
-  // updateVGADisplay(tempCreatePixelData());
-  print(readJtag());
+  updateVGADisplay();
 }
