bcba6da2357ed41c6ea2bcf2ab68aa62707a9d99
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-16 10:55:25 +0100

    test jtag read

--- Board Test Environment/vgaProgram.c
+++ Board Test Environment/vgaProgram.c
@@ -11,8 +11,9 @@ void labinit(void)
   // enable_interrupt();
 }
 
-uint8_t tempCreatePixelData(){
-  uint8_t tempPixelData[320*240];
+uint8_t tempCreatePixelData()
+{
+  uint8_t tempPixelData[320 * 240];
   for (int i = 0; i < 320 * 240; i++)
   {
     tempPixelData[i] = i;
@@ -20,7 +21,8 @@ uint8_t tempCreatePixelData(){
   return *tempPixelData;
 }
 
-void updateVGADisplay(uint8_t *pixelData){
+void updateVGADisplay(uint8_t *pixelData)
+{
   volatile char *VGA = (volatile char *)0x08000000;
   for (int i = 0; i < 320 * 240; i++)
   {
@@ -28,8 +30,15 @@ void updateVGADisplay(uint8_t *pixelData){
   }
 }
 
+char readJtag()
+{
+  volatile char *JTAG = (volatile char *)0x04000040;
+  return JTAG[0];
+}
+
 /* Your code goes into main as well as any needed functions. */
 int main(void)
 {
-  updateVGADisplay(tempCreatePixelData());
+  // updateVGADisplay(tempCreatePixelData());
+  print(readJtag());
 }
