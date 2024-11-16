f0da3608aac0a4a614cfcd21b3f46bd046492b00
Finn Harkort-Berge <finnharkort@gmail.com>
2024-11-16 15:54:14 +0100

    try changed color on input img

--- Board Test Environment/vgaProgram.c
+++ Board Test Environment/vgaProgram.c
@@ -6,8 +6,8 @@ extern void print_dec(unsigned int);
 extern void display_string(char *);
 extern void enable_interrupt(void);
 
-void handle_interrupt(){
-
+void handle_interrupt()
+{
 }
 
 /* Add your code here for initializing interrupts. */
@@ -19,12 +19,12 @@ void labinit(void)
 void updateVGADisplay()
 {
   volatile char *VGA = (volatile char *)0x08000000;
-    
-    // Loop through every pixel in the screen (320x240)
-    for (int i = 65; i < (320 * 240)+65; i++)
-    {
-        VGA[i-65] = flowers4_bmp[i];  
-    }
+
+  // Loop through every pixel in the screen (320x240)
+  for (int i = 65; i < (320 * 240) + 65; i++)
+  {
+    VGA[i - 65] = flowers_bmp[i];
+  }
 }
 
 /* Your code goes into main as well as any needed functions. */
