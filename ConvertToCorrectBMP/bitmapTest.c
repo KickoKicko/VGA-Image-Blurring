8560f5cc2584e0f3bb75a7c1051b915e45777554
Finn Harkort-Berge <finnharkort@gmail.com>
2024-12-02 16:29:17 +0100

    add hardcoded gaussian matrices

--- ConvertToCorrectBMP/bitmapTest.c
+++ ConvertToCorrectBMP/bitmapTest.c
@@ -136,18 +136,114 @@ void generate332Palette(RGBQUAD *palette)
     }
 }
 
+int fakeGenerateGaussianKernel1(int *filterMatrix){
+    filterMatrix[0] = 1;
+    filterMatrix[1] = 2;
+    filterMatrix[2] = 1;
+    filterMatrix[3] = 2;
+    filterMatrix[4] = 4;
+    filterMatrix[5] = 2;
+    filterMatrix[6] = 1;
+    filterMatrix[7] = 2;
+    filterMatrix[8] = 1;
+    return 16;
+}
+
+int fakeGenerateGaussianKernel2(int *filterMatrix){
+    filterMatrix[0] = 1;
+    filterMatrix[1] = 8;
+    filterMatrix[2] = 17;
+    filterMatrix[3] = 8;
+    filterMatrix[4] = 1;
+    filterMatrix[5] = 8;
+    filterMatrix[6] = 70;
+    filterMatrix[7] = 141;
+    filterMatrix[8] = 70;
+    filterMatrix[9] = 8;
+    filterMatrix[10] = 17;
+    filterMatrix[11] = 141;
+    filterMatrix[12] = 286;
+    filterMatrix[13] = 141;
+    filterMatrix[14] = 17;
+    filterMatrix[15] = 8;
+    filterMatrix[16] = 70;
+    filterMatrix[17] = 141;
+    filterMatrix[18] = 70;
+    filterMatrix[19] = 8;
+    filterMatrix[20] = 1;
+    filterMatrix[21] = 8;
+    filterMatrix[22] = 17;
+    filterMatrix[23] = 8;
+    filterMatrix[24] = 1;
+    return 1266;
+}
+
+int fakeGenerateGaussianKernel3(int *filterMatrix){
+    filterMatrix[0] = 1;
+    filterMatrix[1] = 34;
+    filterMatrix[2] = 286;
+    filterMatrix[3] = 581;
+    filterMatrix[4] = 286;
+    filterMatrix[5] = 34;
+    filterMatrix[6] = 1;
+    filterMatrix[7] = 34;
+    filterMatrix[8] = 1177;
+    filterMatrix[9] = 9822;
+    filterMatrix[10] = 19920;
+    filterMatrix[11] = 9822;
+    filterMatrix[12] = 1177;
+    filterMatrix[13] = 34;
+    filterMatrix[14] = 286;
+    filterMatrix[15] = 9822;
+    filterMatrix[16] = 81937;
+    filterMatrix[17] = 166178;
+    filterMatrix[18] = 81937;
+    filterMatrix[19] = 9822;
+    filterMatrix[20] = 286;
+    filterMatrix[21] = 581;
+    filterMatrix[22] = 19920;
+    filterMatrix[23] = 166178;
+    filterMatrix[24] = 337028;
+    filterMatrix[25] = 166178;
+    filterMatrix[26] = 19920;
+    filterMatrix[27] = 581;
+    filterMatrix[28] = 286;
+    filterMatrix[29] = 9822;
+    filterMatrix[30] = 81937;
+    filterMatrix[31] = 166178;
+    filterMatrix[32] = 81937;
+    filterMatrix[33] = 9822;
+    filterMatrix[34] = 286;
+    filterMatrix[35] = 34;
+    filterMatrix[36] = 1177;
+    filterMatrix[37] = 9822;
+    filterMatrix[38] = 19920;
+    filterMatrix[39] = 9822;
+    filterMatrix[40] = 1177;
+    filterMatrix[41] = 34;
+    filterMatrix[42] = 1;
+    filterMatrix[43] = 34;
+    filterMatrix[44] = 286;
+    filterMatrix[45] = 581;
+    filterMatrix[46] = 286;
+    filterMatrix[47] = 34;
+    filterMatrix[48] = 1;
+    return 1497340;
+}
+
+
 int generateGaussianKernel(int kernelRadie, int *filterMatrix)
 {
     double sigma = 0.84089642;
     int sum = 0;
     int count = 0;
     // Generate the kernel values
-    double multiplier = 1/(pow(M_EULER,(-((kernelRadie * kernelRadie)*2) / (2 * sigma * sigma)))/(2 * M_PI * sigma * sigma));
+    //double multiplier = 1/(pow(M_EULER,(-((kernelRadie * kernelRadie)*2) / (2 * sigma * sigma)))/(2 * M_PI * sigma * sigma));
     for (int y = -kernelRadie; y <= kernelRadie; y++)
     {
         for (int x = -kernelRadie; x <= kernelRadie; x++)
         {
-            filterMatrix[count] = roundf(multiplier*pow(M_EULER,(-((x * x) + (y * y)) / (2 * sigma * sigma)))/(2 * M_PI * sigma * sigma));
+            //filterMatrix[count] = roundf(multiplier*pow(M_EULER,(-((x * x) + (y * y)) / (2 * sigma * sigma)))/(2 * M_PI * sigma * sigma));
             sum += filterMatrix[count];
             count++;
         }
@@ -210,7 +306,15 @@ int *matrixGenerator(int blurType, int kernelSize, int kernelRadie, int *sumPoin
     }
     else if (blurType == 1) // GaussianBlur
     {
-        *sumPointer = generateGaussianKernel(kernelRadie, filterMatrix);
+        if(kernelRadie == 1){// All of these three functions return hardcoded gaussian matrices since the board can not use float values which is needed for the gaussian calculations.
+            *sumPointer = fakeGenerateGaussianKernel1(filterMatrix);
+        }
+        else if(kernelRadie == 2){
+            *sumPointer = fakeGenerateGaussianKernel2(filterMatrix);
+        }
+        else if(kernelRadie == 3){
+            *sumPointer = fakeGenerateGaussianKernel3(filterMatrix);
+        }
     }
     else if (blurType == 2) // Sharpen
     {
